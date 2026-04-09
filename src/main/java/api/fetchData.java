71f6dfb659e8828808ca5e92fcc0d927d654b00d
tsteller03 <tsteller03@icloud.com>
2026-04-09 14:14:08 -0400

    Added outline for all data that can be fetched

--- src/main/java/api/fetchData.java
+++ src/main/java/api/fetchData.java
@@ -9,10 +9,33 @@ public class fetchData {
     public static void main(String[] args) throws Exception {
         String key = apiKey.getApiKey();
         String symbol = "AAPL";
+        String interval = "1min"; // 1min, 5min, 15min, 30min, 45min, 1h, 2h, 4h, 5h, 1day, 1week, 1month
 
-        // this only calls price for now
-        String website = "https://api.twelvedata.com/price?symbol=" + symbol + "&apikey=" + key;
-        
+        // maybe just split these into all separate functions/files? >_<
+
+        // will also need tests
+
+        // get current price
+        // String website = "https://api.twelvedata.com/price?symbol=" + symbol + "&apikey=" + key;
+
+        // get time series (rice, currency, open, high, low, change, percent change)
+        //need to print nicely
+        //String website = "https://api.twelvedata.com/time_series?symbol=" + symbol 
+        //+ "&interval=" + interval + "&apikey=" + key;
+
+        // get quote
+        // String website = "https://api.twelvedata.com/quote?symbol=" + symbol + "&apikey=" + key;
+
+        // get eod price
+        // String website = "https://api.twelvedata.com/eod?symbol=" + symbol + "&apikey=" + key;
+
+        // symbol search
+        String website = "https://api.twelvedata.com/symbol_search?symbol=" + symbol + "&apikey=" + key;
+
+        // possibly add market research + exchange schedule and asset catalog functionality?
+
+        // note: limitations are largely through api, 800 credits a day and we are using free version
+
         URI uri = new URI(website);
         URL url = uri.toURL();
 
@@ -21,7 +44,7 @@ public class fetchData {
 
         Scanner input = new Scanner(urlConnection.getInputStream());
 
-        while (input.hasNext()) {
+        while (input.hasNext()) { // fix so this prints out nicely
             System.out.println(input.nextLine());
         }
 
