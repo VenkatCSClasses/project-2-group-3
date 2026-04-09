a7f1cef107850e40e66364deac40d58f37132201
tsteller03 <tsteller03@icloud.com>
2026-04-09 16:21:43 -0400

    Add barebones tests

--- src/test/java/api/apiTest.java
+++ src/test/java/api/apiTest.java
@@ -1,5 +1,27 @@
 package api;
 
-public class apiTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/*
+TO-DO
+Implement proper JSON input formatting in main code
+    This will ensure proper tests can be written
+Add integration tests
+*/
+
+class apiTest {
+
+    @Test
+    // Ensure API key is properly fetched
+    void getApiKeyTest() {
+        assertEquals("d355b76586984b69a33ce709af165e84", apiKey.getApiKey());
+    }
+
+    @Test
+    // Ensure stock symbol is properly fetched
+    void getSymbolTest() {
+        assertEquals("AAPL", Symbol.getSymbol());
+    }
     
 }
