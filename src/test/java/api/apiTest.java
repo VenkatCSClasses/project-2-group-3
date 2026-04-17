6db5fbdd6a10f8b56d34fa325e70cc80665a12c1
tsteller03 <tsteller03@icloud.com>
2026-04-17 10:39:21 -0400

    Ignore

--- src/test/java/api/apiTest.java
+++ src/test/java/api/apiTest.java
@@ -3,25 +3,12 @@ package api;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-/*
-TO-DO
-Implement proper JSON input formatting in main code
-    This will ensure proper tests can be written
-Add integration tests
-*/
-
-class apiTest {
+class ApiTest {
 
     @Test
     // Ensure API key is properly fetched
     void getApiKeyTest() {
-        assertEquals("d355b76586984b69a33ce709af165e84", apiKey.getApiKey());
-    }
-
-    @Test
-    // Ensure stock symbol is properly fetched
-    void getSymbolTest() {
-        assertEquals("AAPL", Symbol.getSymbol());
+        assertEquals("d355b76586984b69a33ce709af165e84", ApiKey.getApiKey());
     }
     
 }
